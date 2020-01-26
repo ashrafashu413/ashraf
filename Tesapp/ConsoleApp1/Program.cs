3cd2fc247e5e610893f35062e657b520d880537e
Ashraf B <ashrafashu413@outlook.com>
2020-01-26 17:18:07 +0530

    changes to console

--- Tesapp/ConsoleApp1/Program.cs
+++ Tesapp/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello First app");
+            Console.WriteLine("Hello First app test");
             Console.ReadLine();
         }
     }
